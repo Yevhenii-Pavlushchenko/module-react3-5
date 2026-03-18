fb7da262b43dc63fe6b09fcf9d7e33e39708f079
JustFun <paw93@ukr.net>
2026-03-18 12:47:42 +0200

    end http

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,37 @@
 
-import axios from 'axios';
-import './App.css'
+import "./App.css";
 // import OrderForm from './components/OrderForm/OrderForm';
-import SearchForm from './SearchForm/SearchForm';
-import { useState } from 'react';
-import ArticleList from './ArticleList/ArticleList';
-import type { Article } from '../types/article';
+import SearchForm from "./SearchForm/SearchForm";
+import { useState } from "react";
+import ArticleList from "./ArticleList/ArticleList";
+import type { Article } from "../types/article";
+import { fetchArticles } from "../services/articleService";
 
-
-interface ArticleHttpResponse {
-  hits: Article[]
-}
 
 
 function App() {
-
-// const hadleOrder = (data: string) => {
-//   console.log("Order received from:", data);
+  // const hadleOrder = (data: string) => {
+  //   console.log("Order received from:", data);
   // }
-  
-  const [articles, setArticles] = useState<Article[]>([]);
 
+  const [articles, setArticles] = useState<Article[]>([]);
   const [isLoading, setIsLoading] = useState(false);
-  
+  const [isError, setIsError] = useState(false);
+
   const handlerSearch = async (topic: string) => {
-    setIsLoading(true);
-    
-    const responce = await axios.get<ArticleHttpResponse>(`https://hn.algolia.com/api/v1/search?query=${topic}`)
-    
-    setIsLoading(false);
-    setArticles(responce.data.hits);
-  }
+    try {
+      setIsLoading(true);
+      setIsError(false);
+
+      const data = await fetchArticles(topic);
+      
+      setArticles(data);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <>
@@ -40,10 +41,10 @@ function App() {
       <h1>Search Form</h1>
       <SearchForm onSubmit={handlerSearch} />
       {isLoading && <p>Loading...</p>}
+      {isError && <p>Something went wrong...</p>}
       {articles.length > 0 && <ArticleList hits={articles} />}
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
