d21ad17623fa27f25cb50c4c579bda522b5601ae
JustFun <paw93@ukr.net>
2026-03-18 15:16:42 +0200

    before async

--- src/components/SideEffects/SideEffects.tsx
+++ src/components/SideEffects/SideEffects.tsx
@@ -2,18 +2,22 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 export default function SideEffects() {
-  const [person, setPerson] = useState(null);
+    const [person, setPerson] = useState(null);
+    const [count, setCount] = useState(1);
  
     useEffect(() => {
 	  console.log('Effect ran!');
      axios
-      .get('https://swapi.info/api/people/2')
-      .then((response) => setPerson(response.data));
-    }, []);
+      .get(`https://swapi.info/api/people/${count}`)
+      .then((response) => setPerson(response.data.name));
+    }, [count]);
+
      console.log('App rendred!');
 
   return (
-    <>
+      <>
+        <h2>The count is {count}</h2>
+        <button onClick={() => setCount(count + 1)}>Get next character {count}</button>
       <pre>{JSON.stringify(person, null, 2)}</pre>
     </>
   );
