93092798ea0527493441db328c1594023222a7f9
JustFun <paw93@ukr.net>
2026-03-18 17:19:50 +0200

    +++

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,85 +1,20 @@
 import "./App.css";
 
-import ArticleList from "./ArticleList/ArticleList";
 
-import type { Article } from "../types/article";
-import { fetchArticles } from "../services/articleService";
 
-import { useState, } from "react";
-
-import SearchForm from "./SearchForm/SearchForm";
-import Modal from "./Modal/Modal";
-import SomeUseEffects from "./SomeUseEffects/SomeUseEffects";
-// import LocalStorageBtn from "./LocalStorageBtn/LocalStorageBtn";
-// import SideEffects from "./SideEffects/SideEffects";
-// import OrderForm from "./OrderForm/OrderForm";
-// import Timer from "./ClearEffects/ClearEffects";
 
 function App() {
   //!States
-  const [articles, setArticles] = useState<Article[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  // const [isOpen, setIsOpen] = useState(false);
-  //!Modal
-  const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => setIsOpen(true);
-  const closeModal = () => setIsOpen(false);
-
-
-  //!env
-  // const myEnv = import.meta.env.VITE_API_KEY;
-  // console.log("My env variable:", myEnv);
+  
 
   //!hendlers
-  const handlerSearch = async (topic: string) => {
-    try {
-      setIsLoading(true);
-      setIsError(false);
-
-      const data = await fetchArticles(topic);
-
-      setArticles(data);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  
 
  
 
   return (
     <>
-      <h1>Search Form</h1>
-      <SearchForm onSubmit={handlerSearch} />
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>Something went wrong...</p>}
-      {articles.length > 0 && <ArticleList hits={articles} />}
-
-      {/* <OrderForm /> */}
-
-      {/* <SideEffects /> */}
-
-      {/* <button onClick={() => setIsOpen(!isOpen)}>
-        {isOpen ? "Hide Timer" : "Show Timer"}
-      </button>
-      {isOpen && <Timer />} */}
-
-       <div>
-        <h2>тут будет модалка</h2>
-        <button onClick={openModal}>Open Modal</button>
-        {isOpen &&  <Modal onClose={closeModal} />}
-      </div>
-
-      {/* <div>
-        <h2>Local Storage Button</h2>
-      <LocalStorageBtn/>
-      </div> */}
-
-      <div>
-        <SomeUseEffects/>
-      </div>
+     
     </>
   );
 }
